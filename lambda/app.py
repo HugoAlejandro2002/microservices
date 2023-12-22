e997a3e2c6e88750d37c3047c80beafabfb09667
ElJoamy <josephsalguero1@upb.edu>
2023-12-22 16:29:37 +0000

    Refactor SQL script execution and error handling

--- lambda/app.py
+++ lambda/app.py
@@ -4,21 +4,17 @@ import os
 
 def run_sql_script(file_path, connection):
     try:
-        # Crear un cursor para ejecutar comandos SQL
         cursor = connection.cursor()
 
-        # Leer el script SQL desde el archivo
         with open(file_path, 'r') as file:
             sql_script = file.read()
 
-        # Ejecutar el script SQL
         for result in cursor.execute(sql_script, multi=True):
             if result.with_rows:
                 print("Rows:", result.fetchall())
             else:
                 print("Rows affected:", result.rowcount)
 
-        # Confirmar los cambios
         connection.commit()
 
         print("Script SQL ejecutado con éxito.")
@@ -32,31 +28,28 @@ def run_sql_script(file_path, connection):
 
 def lambda_handler(event, context):
     try:
-        # Conexión a la base de datos y ejecución del script
         connection = mysql.connector.connect(
-            host=os.environ("DB_HOST"),
-            port=os.environ("DB_PORT"),
-            user=os.environ("DB_USER"),
-            password=os.environ("DB_PASS"),
-            database=os.environ("DB_NAME")
+            host=os.environ.get('DB_HOST'),
+            port=os.environ.get('DB_PORT'),
+            user=os.environ.get('DB_USER'),
+            password=os.environ.get('DB_PASS'),
+            database=os.environ.get('DB_NAME')
         )
-        sql_file_path = 'lambda/init.sql'
+        sql_file_path = 'init.sql'
         run_sql_script(sql_file_path, connection)
 
-        # Respuesta exitosa
         return {
-            'statusCode': 200,
-            'headers': {
-                'Content-Type': 'application/json',
+            "statusCode": 200,
+            "headers": {
+                "Content-Type": "application/json",
             },
-            'body': json.dumps({'message': 'Script SQL ejecutado con éxito'})
+            "body": json.dumps({"message": "Script SQL ejecutado con éxito"})
         }
     except Exception as e:
-        # Respuesta en caso de error
         return {
-            'statusCode': 500,
-            'headers': {
-                'Content-Type': 'application/json',
+            "statusCode": 500,
+            "headers": {
+                "Content-Type": "application/json",
             },
-            'body': json.dumps({'error': str(e)})
+            "body": json.dumps({"error": str(e)})
         }
